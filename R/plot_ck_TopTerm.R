f4dcddbe4f5890246d171b2e95ea3119173823e5
CongSIBR <cong.liu@sanofi.com>
2023-11-30 08:45:17 +0800

    plot ck result

--- R/plot_ck_TopTerm.R
+++ R/plot_ck_TopTerm.R
@@ -1,5 +1,46 @@
 
 
 
+#' Title
+#'
+#' @param ck
+#' @param col_n
+#' @param label_format
+#' @param n
+#' @param class
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_ck_topterm <- function(ck, col_n, label_format = 40,
+                                 n = 25, class = "KEGG") {
+  ck_res <- ck@compareClusterResult %>% as_tibble()
 
+  top10_term <- ck_res %>%
+    pivot_wider(
+      id_cols = c(ID, Description), names_from = "Cluster",
+      values_from = "p.adjust"
+    ) %>%
+    rowwise() %>%
+    mutate(range = min(c_across(all_of(col_n)), na.rm = TRUE)) %>%
+    ungroup() %>%
+    arrange(range) %>%
+    slice_head(n = n) %>%
+    pull(Description)
+
+  enrichplot::dotplot(ck,
+                      label_format = label_format,
+                      showCategory = top10_term,
+                      title = glue::glue("{class} TopTerm")
+  ) +
+    theme(axis.text.x = element_text(angle = 90)) +
+    scale_fill_gradientn(
+      colors = RColorBrewer::brewer.pal(11, "RdBu"),
+      breaks = c(0, 0.025, 0.05),
+      na.value = "gray",
+      # labels=c("Minimum",0.05,"Maximum"),
+      limits = c(0, 0.05)
+    )
+}
 
--- R/plot_ck_pvalue.R
+++ R/plot_ck_pvalue.R
@@ -1,6 +1,47 @@
 
 
 
-plot_ck_pvalue <- function(){}
+#' Title
+#'
+#' @param ck
+#' @param class
+#' @param p.adjust
+#' @param qvalue
+#' @param showCategory
+#' @param label_format
+#' @param font.size
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_ck_pvalue <- function(ck, class = "KEGG", p.adjust = 0.05,
+                           qvalue = 0.2, showCategory = 15,
+                           label_format = 40,
+                           font.size = 12
+) {
+  ck_f <- ck %>% dplyr::filter(p.adjust < {{ p.adjust }}, qvalue < {{ qvalue }})
+
+  enrichplot::dotplot(ck_f,
+                      label_format = label_format,
+                      showCategory = showCategory,
+                      group = F,
+                      color = "p.adjust",
+                      by = "geneRatio",
+                      size = "geneRatio",
+                      title = glue::glue("{class} P<0.05"),
+                      font.size = font.size
+                      # label_format = f
+  ) +
+    theme(
+      axis.text.x = element_text(angle = 90),
+      # axis.text.y = element_markdown()
+    ) +
+    scale_fill_gradientn(
+      colors = RColorBrewer::brewer.pal(11, "RdBu"),
+      breaks = c(0, 0.025, 0.05),
+      limits = c(0, 0.05)
+    )
+}
 
 
